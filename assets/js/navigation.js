238a8de7cff5bf69e788d16eadfe8d14eaba40e8
Rayn Jay Mei <161795560+MajorDionysus@users.noreply.github.com>
2025-03-07 11:21:59 +0800

    Create navigation.js

---
+++ assets/js/navigation.js
@@ -0,0 +1,9 @@
+// navigation.js
+document.addEventListener("DOMContentLoaded", function() {
+    const navToggle = document.getElementById('nav-toggle');
+    const navMenu = document.getElementById('nav-menu');
+
+    navToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+});
