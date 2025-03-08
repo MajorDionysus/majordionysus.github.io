565951ebc1d89bf2835b4374908498385afebd9b
Rayn Jay Mei <161795560+MajorDionysus@users.noreply.github.com>
2025-03-08 10:53:39 +0800

    Delete assets/js/navigation.js

--- assets/js/navigation.js
+++
@@ -1,9 +0,0 @@
-// navigation.js
-document.addEventListener("DOMContentLoaded", function() {
-    const navToggle = document.getElementById('nav-toggle');
-    const navMenu = document.getElementById('nav-menu');
-
-    navToggle.addEventListener('click', function() {
-        navMenu.classList.toggle('active');
-    });
-});
