1bb2f2421d3bddb419008c7d2cfc0510cfdc003b
Rayn Jay Mei <161795560+MajorDionysus@users.noreply.github.com>
2025-03-10 16:49:23 +0800

    Create blog_loader.js

---
+++ assets/js/blog_loader.js
@@ -0,0 +1,24 @@
+<!-- Sidebar with Blog Module -->
+<aside class="sidebar">
+    <h1>Renjie Mei</h1>
+    <nav>
+        <ul>
+            <li><a href="index.html" class="active">Home</a></li>
+            <li><a href="content/publications.html">Publications</a></li>
+            <li><a href="content/experiences.html">Research & Projects</a></li>
+            <li><a href="content/life.html">Life Timeline</a></li>
+            <li><a href="content/contact.html">Contact</a></li>
+            <li><a href="content/blog.html">Blog</a></li>
+        </ul>
+    </nav>
+</aside>
+
+<!-- Blog Section -->
+<section class="blog">
+    <h2>Latest Blog Posts</h2>
+    <div id="blog-list" class="blog-cards"></div>
+    <div id="blog-content"></div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+<script src="assets/js/blog-loader.js"></script>
