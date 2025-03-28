0b82195cb9b7e4381cd7a1d6371840da5d81ed22
Rayn Jay Mei <161795560+MajorDionysus@users.noreply.github.com>
2025-03-28 22:27:38 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -378,6 +378,22 @@ function formatExperience(exp) {
 }
 
 /**
+ * 格式化 Life Timeline 数据
+ * @param {Object} event - 单条时间线事件数据
+ * @returns {HTMLElement}
+ */
+function formatLifeEvent(event) {
+    const li = document.createElement('li');
+    li.classList.add('timeline-item');
+    li.innerHTML = `
+        <h3>${event.date}: ${event.title}</h3>
+        ${event.description ? `<p>${event.description}</p>` : ''}
+        ${event.image ? `<img src="../assets/images/${event.image}" alt="${event.title}" class="timeline-image">` : ''}
+    `;
+    return li;
+}
+
+/**
  * 格式化博客卡片数据
  * @param {Object} post - 单篇博客数据
  * @returns {HTMLElement}
